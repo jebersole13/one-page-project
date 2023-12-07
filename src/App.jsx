99a9365d2bb0536659c86314693fc24f572076c6
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-12-07 14:04:57 +0900

    navbar check

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import SignIn from './Components/SignIn'
+import Navbar from './Components/Navbar'
 
 function App() {
 
 
   return (
     <div className='App'>
+<Navbar>
  <SignIn/>
+ 
+ </Navbar>
     </div>
   )
 }

0039f5c4ca70006ccbbd441484fef59fb2889495
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-12-07 11:49:47 +0900

    signin update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-
-
-
+import SignIn from 'Components/SignIn'
 
 function App() {
 
 
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>  
+    <div className='App'>
+ <SignIn/>
     </div>
   )
 }

82f03587150f928efe665602ff1bd1d1251ba904
adawg2010 <jcebersole1@gmail.com>
2023-12-15 07:51:30 +0900

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import Navbar from "./Navbar"
+import Footer from "./Components/Footer"
+import Home from "./Components/Pages/Home"
+
+    
 
 
+    
 
 
 function App() {
-
-
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>  
+    <div className="App">
+      <Navbar/>
+     <Home/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <div>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+<ul className="">
+  <li className=""></li>
+</ul>
+<ul className="">
+  <li className=""></li>
+</ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar

2e1425cbc8cd0c55c71c68f8e6f0292e835bfef2
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-01-10 10:25:20 +0900

    navbar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-
+import Navbar from './Components/Navbar';
 
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+      <Navbar/>
         <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>  
+    
     </div>
   )
 }

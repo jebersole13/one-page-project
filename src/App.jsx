dbd1761bc275b6b7cdde4ac761253a35f49e6a16
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-02-08 08:23:43 +0900

    premade rip out

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import About from './Sections/About';
-import Details from './Sections/Details';
-import Offering from './Sections/Offering';
-import Contact from './Components/Contact';
-
 
 function App() {
   return (
     <div className='App'>
-      <Navbar/>
-        <h1 className="text-3xl font-bold underline">
-      One page Scroller Test
-    </h1>  
 
-<h5>About Section</h5>
-<br />
-<About/>
-
-<h5>Details Section</h5>
-<br />
-<Details/>
-
-<h5>Offering section</h5>
-<br />
-<Offering/>
-   <br />
-<br />
-
-<Contact/>
-<Footer/>
     </div>
   )
 }
